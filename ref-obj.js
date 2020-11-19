a7677ead04a10ffa26a27fd0e150141a7d08457f
bella <bella.royle@gmail.com>
2020-11-19 13:53:43 +0000

    moved all reference objects to their own file

---
+++ ref-obj.js
@@ -0,0 +1,26 @@
+exports.moveRef = {
+    fire: 'ember',
+    grass: 'razor leaf',
+    water: 'bubblebeam',
+    normal: 'tackle'
+}
+exports.typeRef = {
+    charmander: 'fire',
+    bulbasaur: 'grass',
+    squirtle: 'water',
+    eevee: 'normal',
+    vaporeon: 'water',
+    flareon: 'fire',
+    leafeon: 'grass',
+    rattata: 'normal'
+}
+exports.strengthRef = {
+    grass: 'water',
+    fire: 'grass',
+    water: 'fire'
+}
+exports.weaknessRef = {
+    grass: 'fire',
+    fire: 'water',
+    water: 'grass'
+}
